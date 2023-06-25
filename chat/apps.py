a96a3f27c47943b82801854cf6f24105b5a3497e
SaydulMursalinPeash <saydulpeash428@gmail.com>
2023-06-25 23:40:55 +0600

    aerhet

--- chat/apps.py
+++ chat/apps.py
@@ -5,5 +5,4 @@ class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
     def ready(self) -> None:
-        import django
-        django.setup()
+        pass
