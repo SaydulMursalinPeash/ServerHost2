fc8ee0b961a422fd50c21103b618c633bd445fc9
SaydulMursalinPeash <saydulpeash428@gmail.com>
2023-06-25 23:10:12 +0600

    esrhe

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
-
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P2P.settings')
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     def ready(self) -> None:
-        from .models import User,UserManager,AccessToken
+        import django
+        django.setup()
         
