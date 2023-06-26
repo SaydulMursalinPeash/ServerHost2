b3eb49613faeced55fe544b90324a75bf702205b
SaydulMursalinPeash <saydulpeash428@gmail.com>
2023-06-26 23:16:07 +0600

    d5u56

--- P2P/asgi.py
+++ P2P/asgi.py
@@ -10,18 +10,18 @@ from payment import *
 from chat.routings import websocket_urlpatterns
 import django
 from channels.routing import get_default_application
-
+from django.core.wsgi import get_wsgi_application
 from P2P.settings import CHANNEL_LAYERS
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P2P.settings')
-django.setup()
-application = get_default_application()
-'''
+wsgi_app=get_wsgi_application()
+asgi_app=get_asgi_application()
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
-})'''
+})
 
 # Use pre-defined channel layer
 channel_layer = CHANNEL_LAYERS['redis']
