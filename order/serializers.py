2b0894418c5ab080fcbe0d55eebc13068405cb94
SaydulMursalinPeash <saydulpeash428@gmail.com>
2023-07-01 21:00:28 +0600

    ferg

--- order/serializers.py
+++ order/serializers.py
@@ -18,11 +18,11 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ['customer','account_details', 'coin', 'amount','order_email', 'method','state']
 
     def create(self, validated_data):
-        customer = validated_data.pop('customer')
+        customer = validated_data.get('customer')
         print(customer)
-        #customer=User.objects.get(id=customer1)
-        coin = validated_data.pop('coin')
-        #coin=Method.objects.get(id=c)
+        customer=User.objects.get(id=customer1)
+        coin = validated_data.get('coin')
+        coin=Method.objects.get(id=c)
         
         mymodel_instance = Order.objects.create(**validated_data)
         mymodel_instance.customer= customer
