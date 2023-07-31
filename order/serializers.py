367006a078d68b6b2abc3bd2950fab5d5c69f644
Saydul Mursalin Peash <saydulpeash428@gmail.com>
2023-07-31 23:09:18 +0600

    dgeth

--- order/serializers.py
+++ order/serializers.py
@@ -12,7 +12,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['customer','account_details', 'coin', 'amount','order_email', 'method','state']
+        fields = ['customer','account_details', 'coin', 'amount','purpose','order_email', 'method','state']
 
     def validate(self, attrs):
         #cu=User.objects.get(id=attrs.get('customer'))
@@ -34,4 +34,57 @@ class OrderSerializer(serializers.ModelSerializer):
         )
         return order
 
-        
+class BuyOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields=['customer','account_details', 'coin', 'amount','purpose','order_email', 'method','state']
+        def validate(self, attrs):
+        #cu=User.objects.get(id=attrs.get('customer'))
+            ad=attrs.get('account_details')
+            #co=Method.objects.get(id=attrs.get('coin'))
+            print(attrs.get('customer'))
+            am=attrs.get('amount')
+            oe=attrs.get('order_email')
+            pu=attrs.get('purpose')
+            me=attrs.get('method')
+            st=attrs.get('state')
+            order=Order.objects.create(
+                customer=attrs.get('customer'),
+                account_details=ad,
+                coin=attrs.get('coin'),
+                amount=am,
+                purpose=pu,
+                order_email=oe,
+                method=me,
+                state=st
+            )
+            return order   
+
+
+class SellOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields=['customer','account_details', 'coin', 'amount','purpose','trc20_address','bep20_address', 'method','state']
+        def validate(self, attrs):
+        #cu=User.objects.get(id=attrs.get('customer'))
+            ad=attrs.get('account_details')
+            #co=Method.objects.get(id=attrs.get('coin'))
+            print(attrs.get('customer'))
+            am=attrs.get('amount')
+            trc=attrs.get('trc20_address')
+            bep=attrs.get('bep20_address')
+            pu=attrs.get('purpose')
+            me=attrs.get('method')
+            st=attrs.get('state')
+            order=Order.objects.create(
+                customer=attrs.get('customer'),
+                account_details=ad,
+                coin=attrs.get('coin'),
+                amount=am,
+                purpose=pu,
+                trc20_address=trc,
+                bep20_address=bep,
+                method=me,
+                state=st
+            )
+            return order 
--- order/urls.py
+++ order/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create-order/',Order.as_view(),name='create_order'),
+    path('create-order/buy/',BuyOrder.as_view(),name='create_order_buy'),
     path('get-user-order/<user_uid>/<coin_id>/',LatestUserOrder.as_view(),name='latest_user_order'),
     path('edit-order/<order_id>/',EditOrderView.as_view(),name='edit_order'),
     path('get-all-order/',GetAllOrdersView.as_view(),name='get_all_order'),
+    path('create-order/sell/',SellOrder.as_view(),name='create_order_sell'),
 ]
